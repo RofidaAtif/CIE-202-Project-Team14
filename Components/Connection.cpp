ac834784798e507e780f7bf0c1c15d42627cde11
HagarAlaaeldien <95480321+HagarAlaaeldien@users.noreply.github.com>
2021-12-05 16:20:13 +0200

    Update Connection.cpp

--- Components/Connection.cpp
+++ Components/Connection.cpp
@@ -1,14 +1,15 @@
 #include "Connection.h"
 
-Connection::Connection(GraphicsInfo *r_GfxInfo, Component *cmp1, Component *cmp2)
+Connection::Connection(int length,GraphicsInfo *r_GfxInfo, Component *cmp1, Component *cmp2)
 {
 	pGfxInfo = r_GfxInfo;
 	Cmpnt1 = cmp1;
 	Cmpnt2 = cmp2;
+	connLength = length;
 }
 void Connection::Draw(UI* pUI)
 {
-	pUI->DrawConnection(*pGfxInfo);
+	pUI->DrawConnection(*pGfxInfo, connLength);
 }
 
 
