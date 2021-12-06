24fabdf6e2a594dcd4a84f7f6e03b55bce47ab1d
HagarAlaaeldien <95480321+HagarAlaaeldien@users.noreply.github.com>
2021-12-06 16:12:00 +0200

    Update Connection.h

--- Components/Connection.h
+++ Components/Connection.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../UI/UI.h"
 
+
+
 class Component;	//forward class declaration
 
 class Connection 
@@ -9,11 +11,12 @@ class Connection
 	Component	*Cmpnt1, *Cmpnt2;
 	GraphicsInfo *pGfxInfo;	//The parameters required to draw a connection
 	int connLength;
+	bool ConnSelected;
 
 public:
 	Connection(int length, GraphicsInfo *r_GfxInfo, Component *cmp1=nullptr, Component *cmp2=nullptr);
 
 	virtual void Draw(UI* );	//for connection to Draw itself
+	void ConnSelect(int Sx, int Sy);
 	
-
 };
