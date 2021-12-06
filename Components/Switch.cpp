2be03a8c5b25009751f7f053b5e9fade9f8224da
HagarAlaaeldien <95480321+HagarAlaaeldien@users.noreply.github.com>
2021-12-06 16:15:39 +0200

    Update Switch.cpp

--- Components/Switch.cpp
+++ Components/Switch.cpp
@@ -6,7 +6,7 @@ Switch::Switch(GraphicsInfo* r_GfxInfo) :Component(r_GfxInfo)
 void Switch::Draw(UI* pUI)
 {
 	//Call output class and pass resistor drawing info to it.
-	pUI->DrawSwitch(*m_pGfxInfo); //update to draw resistor
+	pUI->DrawSwitch(*m_pGfxInfo, GetCompSelected()); //update to draw resistor
 
 }
 
