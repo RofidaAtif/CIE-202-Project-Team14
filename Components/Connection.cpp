df4e64764779799bc37a7b692eac13998600b63f
HagarAlaaeldien <95480321+HagarAlaaeldien@users.noreply.github.com>
2021-12-07 01:50:40 +0200

    Update Connection.cpp

--- Components/Connection.cpp
+++ Components/Connection.cpp
@@ -1,11 +1,12 @@
 #include "Connection.h"
+#include <iostream>
 
-Connection::Connection(int length,GraphicsInfo *r_GfxInfo, Component *cmp1, Component *cmp2)
+
+Connection::Connection( GraphicsInfo* r_GfxInfo, Component* cmp1, Component* cmp2)
 {
 	pGfxInfo = r_GfxInfo;
 	Cmpnt1 = cmp1;
 	Cmpnt2 = cmp2;
-	connLength = length;
 	ConnSelected = false;
 
 }
@@ -13,9 +14,10 @@ Connection::Connection(int length,GraphicsInfo *r_GfxInfo, Component *cmp1, Comp
 void Connection::Draw(UI* pUI)
 {
 	
-	pUI->DrawConnection(*pGfxInfo, connLength,ConnSelected);
+	pUI->DrawConnection(*pGfxInfo,ConnSelected);
 }
 
+/*
 void Connection::ConnSelect(int Sx, int Sy)
 {
 
@@ -25,6 +27,29 @@ void Connection::ConnSelect(int Sx, int Sy)
 	int y2 = pGfxInfo->PointsList[1].y;
 
 	if (Sx <= x2 && Sx >= x1 && Sy <= y2 && Sy >= y1)
+	{
+		ConnSelected = !ConnSelected;
+	}
+	else
+	{
+		ConnSelected = true;
+	}
+	*/
+void Connection::ConnSelect(int Sx, int Sy)
+{
+
+	double x1 = pGfxInfo->PointsList[0].x;
+	double y1 = pGfxInfo->PointsList[0].y;
+	double x2 = pGfxInfo->PointsList[1].x;
+	double y2 = pGfxInfo->PointsList[1].y;
+	double m = abs((y2 - y1) / (x2 - x1));
+	double n =abs( (Sy - y1) / (Sx - x1) );
+	cout << x1 << " " << y1 << " " << x2 << " " << y2 << "\n";
+
+	cout << Sx << " " << Sy<<" "<<m<<"\n";
+	cout << n<<"\n";
+
+	if (  n >= (m - 0.09) && n <= (m + 0.09) && ( (Sx <= x2 && Sx >= x1 && ( (Sy <= y2 && Sy >= y1) || (Sy <= y1 && Sy >= y2))) ) )
 	{
 		ConnSelected = !ConnSelected;
 	}
