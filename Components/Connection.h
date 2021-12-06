f37c75f29bce8f197f4469edb7bf81889993a1aa
HagarAlaaeldien <95480321+HagarAlaaeldien@users.noreply.github.com>
2021-12-07 01:49:37 +0200

    Update Connection.h

--- Components/Connection.h
+++ Components/Connection.h
@@ -10,11 +10,10 @@ class Connection
 	//connection connects between two compoenets
 	Component	*Cmpnt1, *Cmpnt2;
 	GraphicsInfo *pGfxInfo;	//The parameters required to draw a connection
-	int connLength;
 	bool ConnSelected;
 
 public:
-	Connection(int length, GraphicsInfo *r_GfxInfo, Component *cmp1=nullptr, Component *cmp2=nullptr);
+	Connection(GraphicsInfo* r_GfxInfo, Component* cmp1 = nullptr, Component* cmp2 = nullptr);
 
 	virtual void Draw(UI* );	//for connection to Draw itself
 	void ConnSelect(int Sx, int Sy);
