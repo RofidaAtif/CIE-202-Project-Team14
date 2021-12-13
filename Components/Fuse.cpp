a6d9b618e626df95f709bd27aa31b634633204af
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-13 23:57:52 +0200

    Update Fuse.cpp

--- Components/Fuse.cpp
+++ Components/Fuse.cpp
@@ -1,15 +1,20 @@
 #include "Fuse.h"
 
-Fuse::Fuse(GraphicsInfo* r_GfxInfo) :Component(r_GfxInfo)
-{}
+Fuse::Fuse(GraphicsInfo *r_GfxInfo, int id, double res, string lbl):Component(r_GfxInfo, id,res, lbl)
+{
+}
 
 void Fuse::Draw(UI* pUI)
 {
 	//Call output class and pass resistor drawing info to it.
 	pUI->DrawFuse(*m_pGfxInfo); //update to draw resistor
-
+	pUI->DrawLabel(*m_pGfxInfo, GetLabel());
 }
+void Resistor::SaveFile(fstream& f) {
+	GraphicsInfo& Gfx = *m_pGfxInfo;
 
+	f << "RES" << "       " << to_string(GetID()) << "       " << GetLabel() << "       " << GetValue() << "       " << to_string(Gfx.PointsList[0].x) << "       " << to_string(Gfx.PointsList[0].y) << endl;
+}
 void Fuse::Operate()
 {
 
