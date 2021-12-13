b2e30c5da562848d6717bd1091f5925ee76c180b
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-14 00:50:05 +0200

    Update ActionAddLamp.h

--- Actions/ActionAddLamp.h
+++ Actions/ActionAddLamp.h
@@ -10,6 +10,9 @@ private:
 	//Parameters for rectangular area to be occupied by the comp
 	int Cx, Cy;	//Center point of the comp
 	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+	string LBL;
+	double IRES;
+	
 public:
 	ActionAddLamp(ApplicationManager* pApp);
 	virtual ~ActionAddLamp(void);
