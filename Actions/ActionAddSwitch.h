e1c536cd87cbbf0deab95542b070822725ef010c
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-14 00:56:45 +0200

    Update ActionAddSwitch.h

--- Actions/ActionAddSwitch.h
+++ Actions/ActionAddSwitch.h
@@ -10,6 +10,7 @@ private:
 	//Parameters for rectangular area to be occupied by the comp
 	int Cx, Cy;	//Center point of the comp
 	int x1, y1, x2, y2;	//Two corners of the rectangluar area
+	string LBL; // Label of the switch
 public:
 	ActionAddSwitch(ApplicationManager* pApp);
 	virtual ~ActionAddSwitch(void);
