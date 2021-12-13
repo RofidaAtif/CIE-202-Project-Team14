b3b7f685c9b6a0a11de8deee9bcafa37a1529003
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-13 23:50:28 +0200

    Update Resistor.h

--- Components/Resistor.h
+++ Components/Resistor.h
@@ -4,8 +4,8 @@
 class Resistor:public Component
 {
 public:
-	Resistor(GraphicsInfo *r_GfxInfo);
+	Resistor(GraphicsInfo *r_GfxInfo,int id, double res = 0, string lbl = "RES");
 	virtual void Operate();	//Calculates the volt on both terminals
 	virtual void Draw(UI*);	//Draws the resistor
-
+	virtual void SaveFile(fstream& f);
 };
