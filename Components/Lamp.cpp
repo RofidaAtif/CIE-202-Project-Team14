7ed7703ef053d3d07891d82c120f8ee4bdceea42
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-13 23:47:52 +0200

    Update Lamp.cpp

--- Components/Lamp.cpp
+++ Components/Lamp.cpp
@@ -7,9 +7,13 @@ void Lamp::Draw(UI* pUI)
 {
 	//Call output class and pass resistor drawing info to it.
 	pUI->DrawLamp(*m_pGfxInfo, GetCompSelected()); //update to draw resistor
-
+	pUI->DrawLabel(*m_pGfxInfo,GetLabel());
 }
-
+void Lamp::SaveFile(fstream& f) {
+	GraphicsInfo& Gfx = *m_pGfxInfo;
+	
+	f <<"BLB"<<"       "<<to_string(GetID()) << "       " << GetLabel() << "       " << GetValue()  << "       " << to_string( Gfx.PointsList[0].x)<< "       " << to_string(Gfx.PointsList[0].y)<<endl;
+}
 void Lamp::Operate()
 {
 
