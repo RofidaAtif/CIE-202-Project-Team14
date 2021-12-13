fa272bba84316e47cb3a7ac077fb0e919aabefbf
Kareem Mahmoud <59833251+Kareem-zc@users.noreply.github.com>
2021-12-14 00:05:39 +0200

    Update Ground.h

--- Components/Ground.h
+++ Components/Ground.h
@@ -4,8 +4,8 @@
 class Ground :public Component
 {
 public:
-	Ground(GraphicsInfo* r_GfxInfo);
+	Ground(GraphicsInfo *r_GfxInfo,int id, double res = 0, string lbl = "GROUND");
 	virtual void Operate();	//Calculates the volt on both terminals
 	virtual void Draw(UI*);	//Draws the resistor
-
+	virtual void SaveFile(fstream& f);
 };
